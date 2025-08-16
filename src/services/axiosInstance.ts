3854fe3e38ed49fd53ceab8e063721bc12bd1b29
millerbermeo <millerbermeo30@gmail.com>
2025-08-15 22:34:38 -0500

    commit ba

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "http://tuclick.vozipcolombia.net.co/motos/back",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   
   headers: {
